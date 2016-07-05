517daf3fc7c1f6502a0f51adf4565557783bc5c5
Sarah-Belle Laplace <sblaplace@gmail.com>
2016-07-05 09:39:36 -0500

    Add unit test and functionality for checking command validity

--- discordbot.js
+++ discordbot.js
@@ -1,16 +1,28 @@
-var authfile = require('./auth.json');
+var authFile = require('./auth.json');
 var Discord = require('discord.js');
 
 var loadAuthDetails = function(detailKey) {
-    return authfile[detailKey];
+    return authFile[detailKey];
+};
+
+var parseMessage = function(msgContent) {
+  var commands = ["!giveToken", "!listToken"];
+  if (commands.includes(msgContent)){
+      return msgContent;
+  }
 };
 
 exports.loadAuthDetails = loadAuthDetails;
+exports.parseMessage = parseMessage;
 
 var bot = new Discord.Client();
 
 bot.on("ready", function(){
     console.log("I'm ready!");
   });
+
+bot.on("message", function(msg) {
+
+});
 
 bot.loginWithToken(loadAuthDetails("loginToken"));
--- test/bottest.js
+++ test/bottest.js
@@ -1,19 +1,20 @@
 var expect = require("chai").expect;
-var discordbot = require("../discordbot");
+var discordBot = require("../discordbot");
 var Discord = require('discord.js');
-var authfile = require('../auth.json');
 
-/*
-describe("Bot Authenticator", function() {
-  var bot = new Discord.Client();
-  it("logs in to Discord", function(done) {
-    bot.loginWithToken(discordbot.loadAuthDetails("loginToken"), null, null, function(error, token){
-      done();
+describe("Bot", function(){
+  /* describe("User List Builder", function() {
+    it("loads Cache of users from server", function() {
     });
-    expect(bot.uptime).to.be.above(0);
-  });
-  /*it("adds itself to a server given invite link", function() {
-    var addRequested = bot.bot.joinServer(bot.inviteLink);
+  }); */
+  describe("Message Parser", function(){
+    it("only accepts commands from array of commands", function() {
+      var goodCommandArg = "!giveToken";
+      var badCommandArg = "!blah";
+      var goodCommand = discordBot.parseMessage(goodCommandArg);
+      var badCommand = discordBot.parseMessage(badCommandArg);
+      expect(goodCommand).to.equal("!giveToken");
+      expect(badCommand).to.be.undefined;
+    });
   });
 });
-*/
