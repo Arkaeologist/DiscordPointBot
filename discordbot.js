d5cdd25f72a3e3fbd40e8b6afd5ec1c79c2b91b0
Sarah-Belle Laplace <sblaplace@gmail.com>
2016-07-06 04:20:45 -0500

    Add logic for deciding which command to execute

--- discordbot.js
+++ discordbot.js
@@ -1,20 +1,24 @@
+/*jshint multistr: true */
 var authFile = require('./auth.json');
 var Discord = require('discord.js');
 var rolesWhichCanGiveToken = ["Admins", "Mods", "Judges"];
+var adminRole = "Admins";
 
 var loadAuthDetails = function(detailKey) {
     return authFile[detailKey];
 };
 
 var parseMessage = function(msgContent) {
-  var commands = ["!giveToken", "!listToken", "!help"];
+  var commands = ["!giveToken", "!listToken", "!help", "!logout", "!restart"];
   var msgContentArray = msgContent.split(" ");
   if (commands.includes(msgContentArray[0])){
-      if(msgContentArray[0] == "!giveToken" && !(Number.isNaN(Number(msgContentArray[1])))) {
+      if(msgContentArray[0] == "!giveToken" &&
+      !(Number.isNaN(Number(msgContentArray[1])))) {
         msgContentArray[1] = Number(msgContentArray[1]);
       }
       return msgContentArray;
   }
+  return false;
 };
 
 var canUseGiveToken = function(roleArray) {
@@ -26,19 +30,57 @@ var canUseGiveToken = function(roleArray) {
   return false;
 };
 
+var giveToken = function() {
+  return null;
+};
+
+var listToken = function() {
+  return null;
+};
 exports.loadAuthDetails = loadAuthDetails;
 exports.parseMessage = parseMessage;
 exports.canUseGiveToken = canUseGiveToken;
+exports.giveToken = giveToken;
+exports.listToken = listToken;
 
 var bot = new Discord.Client();
 
 bot.on("ready", function(){
     console.log("I'm ready!");
+    bot.setStatus("online", "!help for help");
   });
 
 bot.on("message", function(msg) {
   var parsedMessage = parseMessage(msg.content);
   console.log(parsedMessage);
+  console.log(msg.author.id);
+  console.log(msg.server.rolesOfUser(msg.author.id));
+  console.log(msg.server.rolesOfUser(msg.author.id).includes(adminRole));
+  if (parsedMessage[0] == "!giveToken") {
+    giveToken(parsedMessage);
+  }
+  else if (parsedMessage[0] == "!listToken") {
+    listToken(parsedMessage);
+  }
+  else if (parsedMessage[0] == "!help" || msg.isMentioned(bot.user)) {
+    bot.sendMessage(msg.channel, "Usage of this bot: \n Use !giveToken \
+<number of tokens> <@mention user> to give a user that number of tokens. \
+The number of tokens argument is optional. \n Use !listToken \
+<@mention user> to list that user's tokens, or optionally \
+simply use !listToken to list all users' tokens on the server, \
+formatted to a table sorted by number of tokens. \n \n  \
+Admins only commands: \n Use !logout to cause the bot to go \
+offline. \n Use !restart to restart the bot.");
+  }
+  else if (parsedMessage[0] == "!logout" &&
+    msg.server.rolesOfUser(msg.author.id).includes(adminRole)) {
+      bot.logout();
+  }
+  else if (parsedMessage[0] == "!restart" &&
+    msg.server.rolesOfUser(msg.author.id).includes(adminRole)) {
+      bot.logout();
+      bot.loginWithToken(loadAuthDetails("loginToken"));
+  }
 });
 
 bot.loginWithToken(loadAuthDetails("loginToken"));
--- test/bottest.js
+++ test/bottest.js
@@ -14,7 +14,7 @@ describe("Bot", function(){
       var goodCommand = discordBot.parseMessage(goodCommandArg);
       var badCommand = discordBot.parseMessage(badCommandArg);
       expect(goodCommand[0]).to.equal("!giveToken");
-      expect(badCommand).to.be.undefined;
+      expect(badCommand).to.be.false;
     });
     it("allows for !giveToken with 2 arguments", function() {
       var goodCommandArg = "!giveToken 5 user";
@@ -26,8 +26,24 @@ describe("Bot", function(){
       var goodCommand = discordBot.parseMessage(goodCommandArg);
       expect(goodCommand).to.deep.equal(["!giveToken", "user"]);
     });
+    it("does not allow partial tokens", function() {
+      var badCommandArg = "!giveToken 1.772 user";
+      expect(badCommand).to.be.false;
+    });
+    it("does not allow non number tokens", function() {
+      expect(badCommand).to.be.false;
+    });
+    it("allows a maximum of two arguments for !giveToken", function() {
+      expect(badCommand).to.be.false;
+    });
+    it("allows a maximum of one argument for !listToken", function() {
+      expect(badCommand).to.be.false;
+    });
+    it("does not allow arguments for !help", function() {
+      expect(badCommand).to.be.false;
+    });
   });
-  describe("Give Token Ability Checker", function() {
+  describe("Give Token Permission Checker", function() {
     it("checks if the sender can give tokens", function() {
       roleArray = ["Admins"];
       canUseGiveToken = discordBot.canUseGiveToken(roleArray);
@@ -37,6 +53,11 @@ describe("Bot", function(){
       roleArray = ["Blah"];
       canUseGiveToken = discordBot.canUseGiveToken(roleArray);
       expect(canUseGiveToken).to.be.false;
+    });
+  });
+  describe("User List Builder", function() {
+    it("builds a list of admins", function() {
+      
     });
   });
 });
