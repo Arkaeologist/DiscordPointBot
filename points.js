82167bc9c30f4360dfe90d387afe9631a716c97a
Sarah-Belle Laplace <sblaplace@gmail.com>
2016-09-03 16:17:21 -0500

    Fix various eslint errors

--- points.js
+++ points.js
@@ -1,18 +1,35 @@
 const bot = require('./discordbot').bot;
 const winston = require('winston');
+const jsonfile = require('jsonfile');
+const logAndProfile = require('./discordbot').logAndProfile;
 
-function givePoint(server, channel, mentions, pointsArrayArg) {
-  winston.profile('givePoint');
-  // pointsArray is an optional argument in the case of only one mention
-  let pointsArray = pointsArrayArg.slice(1) || [1];
-  let serverID = server.id;
-  if (pointsArray.length === mentions.length) {
-    savePoints(server, channel, mentions, pointsArray);
+const pointsFile = './points.json';
+
+function logPointMsg(error) {
+  logAndProfile(error, 'givePoint');
+}
+
+function makeServerList(serverList) {
+  let userMentioned;
+  for (let i in mentions) {
+    userMentioned = serverList[server.id].users[mentions[i].id];
+    userMentioned.points += pointsArray[i];
   }
-  // Send message to chat asking for usable input
-  else if (pointsArray.length !== mentions.length) {
-    makeErrorMessage(channel);
-  }
+  return serverList;
+}
+
+function updateServerList(error, serverList) {
+  if (error) winston.error(error);
+  const serverListRemade = makeServerList(server, channel, mentions, pointsArray);
+  // Write to file and message chat confirming points were given
+  jsonfile.writeFile(pointsFile, serverListRemade, function (error) {
+    if (error) winston.error(error);
+    bot.sendMessage(channel, pointsErrorMessage, logPointMsg(error));
+  });
+}
+
+function savePoints(server, channel, mentions, pointsArray) {
+  jsonfile.readFile(pointsFile, updateServerList(error, serverList));
 };
 
 function sendErrorMessage(channel) {
@@ -21,38 +38,38 @@ function sendErrorMessage(channel) {
   bot.sendMessage(channel, pointsErrorMessage, logPointMsg(error));
 };
 
-function logPointMsg(error) {
-  logAndProfile(error, 'givePoint');
+function givePoint(server, channel, mentions, pointsArrayArg) {
+  winston.profile('givePoint');
+  // pointsArray is an optional argument in the case of only one mention
+  const pointsArray = pointsArrayArg.slice(1) || [1];
+  const serverID = server.id;
+  if (pointsArray.length === mentions.length) {
+    savePoints(server, channel, mentions, pointsArray);
+  }
+  // Send message to chat asking for usable input
+  else if (pointsArray.length !== mentions.length) {
+    sendErrorMessage(channel);
+  }
 };
 
-function savePoints(server, channel, mentions, pointsArray) {
-  let pointsMessage = '';
-  let userMentioned;
-  jsonfile.readFile(pointsFile, function(error, serverList) {
-    if (error) winston.error(error);
-    serverList = makeServerList(server, channel, mentions, pointsArray);
-    // Write to file and message chat confirming points were given
-    jsonfile.writeFile(pointsFile, serverList, function (error) {
-      if (error) winston.error(error);
-      bot.sendMessage(channel, pointsErrorMessage, logPointMsg(error));
-    });
-  });
-};
 
-function makeServerList() {
 
-};
+
+
+
+
+
 
 function makeSaveMessage(server, channel, mentions, pointsArray) {
+  let pointsMessage = '';
   for (let i in mentions) {
-    userMentioned = serverList[server.id].users[mentions[i].id];
-    userMentioned.points += pointsArray[i];
     pointsMessage = pointsMessage.concat('Gave ' + pointsArray[i] + ' ' +
     pointName + 's to ' +
     serverList[server.id].users[mentions[i].id].name + ' \n');
   }
   return serverList;
 };
+
 /* Lists the point values for a server of all users mentioned, or, if
 *  no users are mentioned, then for all users on that server.
 */
